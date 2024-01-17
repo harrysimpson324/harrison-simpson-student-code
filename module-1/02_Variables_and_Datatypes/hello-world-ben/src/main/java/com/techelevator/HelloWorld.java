32be0a0213a7ad6fcf9ee34f1c54395a6a02e584
Ben Langhinrichs <benjamin@techelevator.com>
2024-01-17 12:25:57 -0500

    Lecture-ben, exercises, and hello-world-ben from M1/Day 2

---
+++ module-1/02_Variables_and_Datatypes/hello-world-ben/src/main/java/com/techelevator/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.techelevator;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+//import java.time.LocalTime;
+//import java.time.format.DateTimeFormatter;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+        System.out.println("Today is " + LocalDate.now());
+        System.out.println("The current time is " + LocalTime.now());
+
+    }
+
+}

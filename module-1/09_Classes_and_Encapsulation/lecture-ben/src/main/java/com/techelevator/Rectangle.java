0e1185728a8361b248a97f22c651347d6e7303b9
Ben Langhinrichs <benjamin@techelevator.com>
2024-01-26 12:21:18 -0500

    Exercises and lecture-ben for M1/Day 9, Classes and Encapsulation, and tutorials for Monday

---
+++ module-1/09_Classes_and_Encapsulation/lecture-ben/src/main/java/com/techelevator/Rectangle.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+public class Rectangle {
+    // properties
+    private int length;
+    private int width;
+
+    // constructors
+    public Rectangle(int lengthIn, int widthIn) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // getters and setters
+    public int getArea() {
+        return length * width;
+    }
+
+    // other methods
+
+}

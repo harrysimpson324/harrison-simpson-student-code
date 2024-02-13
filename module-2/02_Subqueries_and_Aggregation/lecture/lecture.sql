8fee15f5b8df0673f1f9c5fac5281d627dd6c1d0
Ben Langhinrichs <benjamin@techelevator.com>
2024-02-13 08:44:52 -0500

    Lecture for M2/Day 2, Aggregation and Subqueries

---
+++ module-2/02_Subqueries_and_Aggregation/lecture/lecture.sql
@@ -0,0 +1,63 @@
+-- CONCATENATING OUTPUTS
+
+-- All city names and their state abbreviation.
+
+
+-- The all parks by name and date established.
+
+
+-- The census region and state name of all states in the West & Midwest sorted in ascending order.
+
+
+
+-- SUBQUERIES
+
+-- List all cities in the western census region
+
+
+-- List all cities in states with "garden" in their nickname
+
+
+-- Get the parks with the smallest and largest areas
+
+
+-- Get the state name and census region for all states with a national park
+
+
+-- Subqueries aren't limited to WHERE clauses, can appear in SELECT list (must return only 1 result)
+-- Include state name rather than the state abbreviation while counting the number of cities in each state,
+--   ordered from most cities to least.
+
+
+
+-- AGGREGATE FUNCTIONS
+
+-- Average population across all the states. Note the use of alias, common with aggregated values.
+
+
+-- Total population in the West and South census regions
+
+
+-- The number of cities with populations greater than 1 million
+
+
+-- The number of state nicknames.
+
+
+-- The area of the smallest and largest parks.
+
+
+
+-- GROUP BY
+
+-- Count the number of cities in each state, ordered from most cities to least.
+
+
+-- Determine the average park area depending upon whether parks allow camping or not.
+
+
+-- Sum of the population of cities in each state ordered by state abbreviation.
+
+
+-- The smallest city population in each state ordered by city population.
+

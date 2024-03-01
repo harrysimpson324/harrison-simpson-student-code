997f479b21a252e260be294c103cccd4057ed0db
Harry Simpson <harry.simpson324@gmail.com>
2024-02-29 19:51:12 -0500

    submitting exercise 14 module 2

--- module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,6 +2,7 @@ package com.techelevator.auctions.controller;
 
 import java.util.List;
 
+import com.techelevator.auctions.exception.DaoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auctions")
@@ -48,9 +51,30 @@ public class AuctionController {
         }
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public Auction create(@RequestBody Auction auction) {
+    public Auction create(@Valid @RequestBody Auction auction) {
         return auctionDao.createAuction(auction);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    public Auction update(@Valid @RequestBody Auction auction, @PathVariable int id) {
+        auction.setId(id);
+        try {
+            return auctionDao.updateAuction(auction);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable int id) {
+        try {
+            auctionDao.deleteAuctionById(id);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
 

71f43dafd406a30e75e91e803d580019061f2bd2
Harry Simpson <harry.simpson324@gmail.com>
2024-03-18 16:03:21 -0400

    submitting module 3 exercise 7 javascript functions (both challenge questions are completed)

--- module-3/06_Intro_to_JavaScript/exercise/challenge-exercises.js
+++ module-3/06_Intro_to_JavaScript/exercise/challenge-exercises.js
@@ -12,6 +12,33 @@
         iqTest("2 2 4 6") → 0 // all numbers are even, therefore there is no position of an odd number
 */
 
+function iqTest(str) {
+    if (str.length === 0) {
+        return 0;
+    }
+    const numArray = str.split(' ');
+    let numEven = 0;
+    let numOdd = 0;
+    for (let i = 0; i < numArray.length; i++) {
+        if (numArray[i]%2 == 1) {
+            numOdd++
+        }
+        else {
+            numEven++
+        }
+    }
+
+    const isOnlyOneEven = (numEven===1);
+    const isOnlyOneOdd = (numOdd ===1);
+
+    for (element of numArray) {
+        if ((isOnlyOneEven && (element%2===0) || (isOnlyOneOdd && (element%2 === 1)))) {
+            return numArray.indexOf(element) + 1;
+        }
+    }
+    return 0;
+}
+
 /*
 2. **titleCase** Write a function that will convert a string into title case, given an optional 
     list of exceptions (minor words). The list of minor words will be given as a string with each 
@@ -28,3 +55,76 @@ argument is unused.
 		titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
         titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 */
+
+function titleCase(title, exceptions) {
+
+    let exceptionsArray = [];
+    if (exceptions !== undefined) {
+        // console.log('splitting exceptions string')
+        exceptionsArray = exceptions.split(' ');
+    }
+
+    // for(element of exceptionsArray) {
+    //     console.log(element + ' ')
+    // }
+
+
+    const titleArray = title.split(' ');
+
+    const returnArray = [capitalize(titleArray[0])];
+
+    for (let i = 1; i < titleArray.length; i++) {
+
+        if (!isInArray(titleArray[i], exceptionsArray)) {
+            // console.log(`in if statement for when word is not in exceptions array. adding${capitalize(titleArray[i])} to the returnArray`);
+            returnArray.push(capitalize(titleArray[i]));
+        }
+        else {
+            // console.log(`in else statement, word is in exceptions array. adding ${titleArray[i].toLowerCase()} to the returnArray`);
+            returnArray.push(titleArray[i].toLowerCase());
+        }
+    }
+
+    let returnString = '';
+
+    // for (element of returnArray) {
+    //     returnString += element + " "
+    // }
+
+    for (let i = 0; i < returnArray.length; i++) {
+        returnString += returnArray[i];
+        if (i != returnArray.length-1) {
+            returnString+=' ';
+        }
+    }
+    
+    return returnString;
+
+}
+
+function isInArray(word, exceptions) {
+    // console.log("in isInArray method")
+    if (exceptions.length == 0) {
+        // console.log('returning false because no exceptions')
+        return false;
+    }
+
+    for (let i = 0; i < exceptions.length; i++) {
+        if (exceptions[i].toUpperCase() == word.toUpperCase()) {
+            // console.log(`word:${word}exceptions[i]:${exceptions[i]}`)
+            return true;
+        }
+    }
+    // console.log('returning false')
+    return false;
+}
+
+function capitalize(word) {
+    word = word.toLowerCase();
+    const firstLetter = word.charAt(0).toUpperCase();
+    if (word.length < 2) { 
+        return firstLetter.toUpperCase();
+    }
+    const restOfWord = word.substring(1, word.length).toLowerCase();
+    return firstLetter.toUpperCase() + restOfWord
+}
--- module-3/06_Intro_to_JavaScript/exercise/exercises.js
+++ module-3/06_Intro_to_JavaScript/exercise/exercises.js
@@ -13,6 +13,13 @@
         }
 */
 
+function sumDouble(num1, num2) {
+	if (num1 === num2) {
+		return (num1+num2) * 2;
+	}
+	return num1+num2;
+}
+
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
     Given 3 int values, return true if 1 or more of them are teen.
@@ -21,6 +28,17 @@
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
+
+function hasTeen(num1, num2, num3) {
+	const one = (num1 >= 13 && num1 <=19);
+	const two = (num2 >= 13 && num2 <=19);
+	const three = (num3 >= 13 && num3 <=19);
+	if (one || two||three) {
+		return true;
+	}
+	return false;
+
+}
 
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
@@ -31,6 +49,16 @@
 		lastDigit(3, 113) → true
 */
 
+function lastDigit(num1, num2) {
+	const oneLast = num1%10;
+	const twoLast = num2%10;
+	if (oneLast === twoLast) {
+		return true;
+	}
+	return false;
+
+}
+
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
     string, otherwise return the empty string.
@@ -40,6 +68,23 @@
         seeColor("blueTimes") → "blue"
 */
 
+function seeColor(str) {
+	if (str.length < 3) {
+		return '';
+	}
+	const doesStartRed = (str.substring(0, 3) === 'red')
+	if (doesStartRed) {
+		return 'red';
+	}
+	if (str.lenth < 4) {
+		return '';
+	}
+	if (str.substring(0,4) === 'blue') {
+		return 'blue';
+	}
+	return '';
+}
+
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, removes
     the even numbers, and returns a new array of just the the odd numbers.
@@ -47,6 +92,18 @@
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
+
+function oddOnly(intArr) {
+	for (let i = 0; i < intArr.length; i++) {
+		if (intArr[i] % 2 === 0) {
+			intArr.splice(i, 1);
+			i--;
+		}
+	}
+	return intArr;
+
+
+}
 
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
@@ -56,6 +113,16 @@
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+
+function frontAgain(str) {
+	if (str.length < 2) {
+		return false;
+	}
+	if (str.substring(0,2) === str.substring(str.length-2, str.length)) {
+		return true;
+	}
+	return false;
+}
 
 /*
 7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
@@ -68,6 +135,14 @@ or false otherwise.
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
+
+function cigarParty(numCigars, isWeekend) {
+	const inRange = (numCigars >= 40 && numCigars <= 60);
+	if (inRange || (numCigars >=40&&isWeekend)) {
+		return true;
+	}
+	return false;
+}
 
 /*
 8. **fizzBuzz** Given a number, return a value according to the following rules:
@@ -83,6 +158,19 @@ In all other cases return the original number.
 	fizzBuzz(8) → 8
 */
 
+function fizzBuzz(num) {
+	if (num%3==0 && num%5==0) {
+		return 'FizzBuzz';
+	}
+	else if (num%3 ==0) {
+		return 'Fizz';
+	}
+	else if (num%5==0) {
+		return 'Buzz';
+	}
+	return num;
+}
+
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
 
@@ -92,6 +180,16 @@ In all other cases return the original number.
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
 
+function filterEvens(intArr) {
+	for (let i = 0; i < intArr.length; i++) {
+		if (intArr[i] % 2 === 1) {
+			intArr.splice(i, 1);
+			i--;
+		}
+	}
+	return intArr;
+}
+
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
 
@@ -100,6 +198,19 @@ In all other cases return the original number.
 	filterBigNumbers([]) → []
 */
 
+function filterBigNumbers(intArr) {
+	const returnArray = [];
+	if (intArr.length == 0) {
+		return returnArray;
+	}
+	for (let i = 0; i < intArr.length; i++) {
+		if (intArr[i] >= 100) {
+			returnArray.push(intArr[i]);
+		}
+	}
+	return returnArray;
+}
+
 /*
 11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
 parameter, `x` passed in.
@@ -107,6 +218,16 @@ parameter, `x` passed in.
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
+
+function filterMultiplesOfX(numArray, divisor) {
+	for (let i = 0; i < numArray.length; i++ ) {
+		if (numArray[i]%divisor !== 0) {
+			numArray.splice(i, 1);
+			i--;
+		}
+	}
+	return numArray;
+}
 
 /*
 12. **createObject** Write a function that creates an object with a property called 
@@ -120,3 +241,10 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+
+function createObject() {
+	const obj = {firstName: 'Harry', lastName: 'Simpson', age: 28};
+	return obj;
+}
+
+

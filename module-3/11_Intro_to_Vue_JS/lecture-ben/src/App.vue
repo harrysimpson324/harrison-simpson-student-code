a12875c3c168852bbaf78dcece4cd9b33c7aaf77
Ben Langhinrichs <benjamin@techelevator.com>
2024-03-25 11:52:22 -0400

    Exercises and lecture-ben for M3/Day 11, and tutorials for Vue Event Handling

---
+++ module-3/11_Intro_to_Vue_JS/lecture-ben/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <product-review></product-review>
+  </div>
+</template>
+
+<script>
+import ProductReview from './components/ProductReview.vue'
+
+export default {
+  components: {
+    ProductReview
+  }
+}
+</script>
+
+<style>
+.main {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

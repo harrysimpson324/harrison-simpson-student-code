09dea96580ffaf6dec390e7a0e1172cc75a6402d
Ben Langhinrichs <benjamin@techelevator.com>
2024-03-27 12:07:14 -0400

    Exercises and lecture-ben for M3/Day 13, and tutorials for Friday

---
+++ module-3/13_Vue_Component_Communication/lecture-ben/src/components/FavoriteSummary.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="well">
+      <span class="amount">{{ numberOfReviews }}</span>
+      How Many Favorites?
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'favorite-summary',
+    computed: {
+      numberOfReviews() {
+        const reviews = this.$store.state.reviews;
+        const matchingReviews = reviews.filter((review) => {
+          return review.favorited;
+        });
+        return matchingReviews.length;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .well {
+    display: inline-block;
+    width: 15%;
+    border: 1px black solid;
+    border-radius: 6px;
+    text-align: center;
+    margin: 0.25rem;
+    padding: 0.25rem;
+  }
+  
+  .amount {
+    color: darkslategray;
+    display: block;
+    font-size: 2.5rem;
+  }
+  
+  .amount:hover {
+    cursor: pointer;
+  }
+  
+  </style>
+  

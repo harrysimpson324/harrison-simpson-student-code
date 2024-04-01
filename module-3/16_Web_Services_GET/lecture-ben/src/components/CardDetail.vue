9b2482133f09b17f737b8115b77b41cccd636a03
Ben Langhinrichs <benjamin@techelevator.com>
2024-04-01 11:29:36 -0400

    Exercises and lecture-ben for M3/Day 16, Web Services (GET)

---
+++ module-3/16_Web_Services_GET/lecture-ben/src/components/CardDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1><img v-bind:src="card.avatar" height="30" /> &nbsp;{{ card.title }}</h1>
+    <p>{{ card.description }}</p>    
+    <comments-list v-bind:comments="card.comments" />
+  </div>
+</template>
+
+<script>
+import CommentsList from '../components/CommentsList.vue';
+
+export default {
+  props: ['card'],
+  components: {
+    CommentsList
+  }
+};
+</script>
